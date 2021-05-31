69017b51cc9670ae48488ea7eb494cde9653111e
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-05-30 19:28:29 -0700

    server running

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const express = require("express");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
   useFindAndModify: false,
@@ -6,7 +7,16 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/pizza-hunt", {
   useUnifiedTopology: true,
 });
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 // Use this to log mongo queries being executed!
 mongoose.set("debug", true);
 
-app.listen();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.use(require("./controllers/routes"));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
