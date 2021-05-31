eb12c99419d714f95c4a9b32d754c474263e33fb
Maria Donaji Velazquez <xunaxidonaji@hotmail.com>
2021-05-30 19:28:15 -0700

    add reaction to though_routes

--- controllers/routes/api/thought_routes.js
+++ controllers/routes/api/thought_routes.js
@@ -6,7 +6,9 @@ const {
   createThought,
   updateThought,
   deleteThought,
-} = require("../../controllers/thought-controller");
+  createReaction,
+  deleteReaction,
+} = require("../../thought_controller");
 
 // Set up GET all and POST at /api/thoughts
 router.route("/").get(getAllThought).post(createThought);
@@ -17,5 +19,11 @@ router
   .get(getThoughtById)
   .put(updateThought)
   .delete(deleteThought);
+
+// Set up POST reactions at /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(createReaction);
+
+// Set up DELETE reaction at /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
